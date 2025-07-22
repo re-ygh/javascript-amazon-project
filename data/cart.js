73af5cffd491cd3ba18e910bc9a84d49cbea67b9
re-ygh <yaghoobi13.reza83@yahoo.com>
2025-07-22 10:56:07 +0330

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,24 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let quantitySelected = 1;
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    if(quantitySelected && quantitySelector.value){
+        quantitySelected = Number(quantitySelector.value);
+    }
+
+    let matchingItem;
+    cart.forEach((cartItem) =>{
+        if (productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+    if (matchingItem){
+        matchingItem.quantity += quantitySelected;
+    }else {
+        cart.push({
+            productId: productId,
+            quantity: quantitySelected
+        });
+    }
+}
