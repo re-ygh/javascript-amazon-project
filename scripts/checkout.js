854c7c6f746002bf7b62d7c77015a5281a0fcce9
re-ygh <yaghoobi13.reza83@yahoo.com>
2025-08-12 10:24:32 +0330

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,12 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import{ renderOrderSummary } from './checkout/orderSummary.js';
 import{ renderPaymentSummary } from './checkout/paymentSummary.js';
-import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
+// import '../data/backend-practice.js';
 
-renderOrderSummary();
 
-renderPaymentSummary();
-
-renderCheckoutHeader();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+});
