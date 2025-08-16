eb448a7c748a647acb218b08bbc64380918b56b4
re-ygh <yaghoobi13.reza83@yahoo.com>
2025-08-16 11:12:14 +0330

    tests codes cleaned

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage,cart } from "../../data/cart.js";
-import { loadProducts,loadProductsFetch } from "../../data/products.js";
+import { loadProductsFetch } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', () => {
 
