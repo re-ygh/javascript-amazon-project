81bb84e2b7486346c0b12e398fb51ab99b57f6c3
re-ygh <yaghoobi13.reza83@yahoo.com>
2025-08-16 08:22:50 +0330

    tracking page made interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -3,6 +3,8 @@ import { products } from "../data/products.js";
 import { orders } from "../data/orders.js";
 import { getDeliveryOption } from "../data/deliveryOptions.js";
 import { loadProductsFetch } from "../data/products.js";
+import dayjs from  'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
 
 
 loadProductsFetch().then(renderOrderTracking);
@@ -37,16 +39,15 @@ function renderOrderTracking() {
             thisOrder = order;
             thisOrder.cart.forEach((cartItem) => {
                 if( cartItem.productId === thisProduct.id){
-                    deliveryOption = cartItem.deliveryDate;
+                    deliveryOption = cartItem.deliveryId;
                 }
             });
         }   
     });
     
-    
     const isoDate = thisOrder.createdAt;
-    const date = new Date(isoDate);
-    let arrivingDate = new Date(date);
+    const createdDate = new Date(isoDate);
+    let arrivingDate = new Date(createdDate);
     const finalDeliveryOption = getDeliveryOption(deliveryOption);
     arrivingDate.setDate
     (arrivingDate.getDate() + finalDeliveryOption.deliveryDays);
@@ -56,7 +57,6 @@ function renderOrderTracking() {
         month: "long",
         day: "numeric"
     });
-    
 
     let quantity;
     thisOrder.cart.forEach((productToFindQuantity) => {
@@ -90,13 +90,13 @@ function renderOrderTracking() {
         <img class="product-image" src="${productImageLink}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label js-Preparing-label">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label js-Shipped-label">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label js-Delivered-label">
             Delivered
           </div>
         </div>
@@ -109,4 +109,36 @@ function renderOrderTracking() {
     
     document.querySelector('.js-order-tracking')
     .innerHTML = orderTrackHTML;  
+
+
+    const currrentDate = dayjs();
+    // تبدیل فاصله‌ها به میلی‌ثانیه
+    const totalDuration = dayjs(arrivingDate).valueOf() - dayjs(createdDate).valueOf();
+    const passedDuration = currrentDate.valueOf() - dayjs(createdDate).valueOf();
+
+    // محاسبه درصد پیشرفت
+    let statusBarPercentage = (passedDuration / totalDuration) * 100;    
+
+    const progressBar = document.querySelector('.progress-bar');
+    progressBar.style.width = `${statusBarPercentage}%`;
+
+    let currentStatusClass = '';
+
+    if (statusBarPercentage < 50) {
+        currentStatusClass = `.js-Preparing-label`;
+    } else if (statusBarPercentage >= 50 && statusBarPercentage < 100) {
+        currentStatusClass = `.js-Shipped-label`;
+    } else if (statusBarPercentage >= 100) {
+        currentStatusClass = `.js-Delivered-label`;
+    }
+
+    // پیدا کردن المان درست بر اساس کلاس
+    let progressLabelContainer = document.querySelector(currentStatusClass);
+
+    // اضافه کردن کلاس هایلایت یا وضعیت
+    if (progressLabelContainer) {
+        progressLabelContainer.classList.add("current-status");
+    }
+
+
 }
